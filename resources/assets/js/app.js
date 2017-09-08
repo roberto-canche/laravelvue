5b06d2839af0e3cda2f7ab5e57739f0e23d0a52a
roberto-canche <roberto.92_c@hotmail.com>
2017-09-08 14:31:57 -0500

    inicializacion del proyecto

---
+++ resources/assets/js/app.js
@@ -0,0 +1,35 @@
+
+import Vue from 'vue';
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+import App from './App.vue';
+import CreateItem from './components/CreateItem.vue';
+import DisplayItem from './components/DisplayItem.vue';
+import EditItem from './components/EditItem.vue';
+
+const routes = [
+    {
+        name: 'CreateItem',
+        path: '/items/create',
+        component: CreateItem
+    },
+    {
+        name: 'DisplayItem',
+        path: '/',
+        component: DisplayItem
+    },
+    {
+        name: 'EditItem',
+        path: '/edit/:id',
+        component: EditItem
+    }
+];
+
+const router = new VueRouter({ mode: 'history', base: __dirname, routes: routes});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
